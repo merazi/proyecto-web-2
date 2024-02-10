417a74a264e3a1bec8928168cb14cb3010e7cff2
jlmg <jlmg3543@gmail.com>
2024-02-10 16:32:26 -0600

    trying out an rss processor

---
+++ public/assets/js/rss-processor.js
@@ -0,0 +1,17 @@
+google.load("feeds", "1");
+
+function initialize() {
+    var feed = new google.feeds.Feed("https://www.nasa.gov/news-release/feed/");
+    feed.load(function(result) {
+        if (!result.error) {
+            var container = document.getElementById("feed");
+            for (var i = 0; i < result.feed.entries.length; i++) {
+		var entry = result.feed.entries[i];
+		var div = document.createElement("div");
+		div.appendChild(document.createTextNode(entry.title));
+		container.appendChild(div);
+            }
+        }
+    });
+}
+google.setOnLoadCallback(initialize);
