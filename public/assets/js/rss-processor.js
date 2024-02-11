3142f770def349c376c9f973fb8e84b4e219a323
jlmg <jlmg3543@gmail.com>
2024-02-10 18:37:20 -0600

    rss feed works now

--- public/assets/js/rss-processor.js
+++ public/assets/js/rss-processor.js
@@ -7,17 +7,20 @@ fetch(RSS_URL)
 	console.log(data);
 	const items = data.querySelectorAll("item");
 	let html = ``;
+	console.log(data.querySelector("link"))
 	items.forEach(el => {
 	    html += `
         <article>
-          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
-          <h2>
+          <!--<img src="${el.querySelector("link").innerHTML}image/large.jpg" alt=""/>-->
+          <h1 style="display: inline-block;">
             <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+              <img src="https://img.icons8.com/color/50/nasa.png" alt="" style="vertical-align: middle;"/>
               ${el.querySelector("title").innerHTML}
             </a>
-          </h2>
+          </h1>
+          <p>${el.querySelector("description").innerHTML}</p
         </article>
       `;
 	});
-	content.insertAdjacentElement("afterbegin", html);
+	document.getElementById("contents").insertAdjacentHTML("beforebegin", html);
     });
